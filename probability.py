b8956357298481e8af65223bb5350ef1d2bc1881
submentula <vslslvv88888888@gmail.com>
2025-05-06 10:29:11 +0300

    Update probability.py

--- probability.py
+++ probability.py
@@ -1,24 +1,13 @@
 def Pa(dc):
-    favorable_outcomes = 0
-    for d1 in range(1, 21):
-        for d2 in range(1, 21):
-            if d1 == 20 or d2 == 20:
-                favorable_outcomes += 1
-                continue
-            if d1 == 1 and d2 == 1:
-                continue
-            if max(d1, d2) >= dc:
-                favorable_outcomes += 1
+    if dc <= 1:
+        favorable_outcomes = 399
+    else:
+        count_less_than_dc_in_1_to_19 = min(19, dc - 1)
+        outcomes_19x19_max_lt_dc = count_less_than_dc_in_1_to_19 ** 2
+        favorable_outcomes = 400 - outcomes_19x19_max_lt_dc
     return favorable_outcomes / 400
 
 def Pb(dc, bonus):
-    if dc - bonus >= 20:
-        return 1 / 20
-    if dc - bonus <= 1:
-        return 19 / 20
-    favorable_outcomes = 0
-    for result in range(1, 21):
-        if result + bonus >= dc:
-            favorable_outcomes += 1
-    return favorable_outcomes / 20
-    
+    threshold = dc - bonus
+    favorable_outcomes_count = max(0, 21 - max(1, threshold))
+    return (favorable_outcomes_count + 1) / 21
